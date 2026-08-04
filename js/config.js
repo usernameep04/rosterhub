16cb37460cc4621544a316c0671888a2953e886e
usernameep04 <usernameep04@gmail.com>
2026-08-04 13:36:18 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -15,3 +15,5 @@ const SUPABASE_URL = "https://mbuabdwfncctdnrizyzj.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_LKA_eYIeH-pJDDYHWRvSBA_yfPWH8wR";
 
 const ADMIN_PASSWORD = "Brian-0824";
+
+const COMMUNITY_URL = "";
